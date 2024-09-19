3660e51018d593ba474befd650644c5591c84caf
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2024-09-19 22:22:59 +0530

    Create 024-Contains-Duplicates.cpp

---
+++ 024-Contains-Duplicates.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for(int num : nums){
+            if(mpp[num] == 1)    return true;
+            mpp[num]++;
+        }    
+        return false;
+    }
+};
