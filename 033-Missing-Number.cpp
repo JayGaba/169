252c3656f0ffb7a685387483112bc8215aad1d7c
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2024-09-28 23:20:34 +0530

    Create 033-Missing-Number.cpp

---
+++ 033-Missing-Number.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/missing-number
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum1 = (n*(n+1))/2;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += nums[i];
+        }
+        return sum1-sum;
+        
+    }
+};
