3733573a4efe9711d9e1cb9c2b41cec667430f25
Jayesh Gaba <111695826+JayGaba@users.noreply.github.com>
2024-10-05 09:15:36 +0530

    Create 044-Min-Stack.cpp

---
+++ 044-Min-Stack.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/min-stack/
+class MinStack {
+public:
+    vector<pair<int, int>>s;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(s.empty())   s.push_back({val, val});
+        else    s.push_back({val, min(val, s.back().second)});
+    }
+    
+    void pop() {
+        s.pop_back();
+    }
+    
+    int top() {
+        return s.back().first;
+    }
+    
+    int getMin() {
+        return s.back().second;
+    }
+};
